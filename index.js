69c0424989f8fab4a2286a09e6166656ee200d2e
Xazg54 <159276443+Xazg54@users.noreply.github.com>
2024-02-07 21:46:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,66 +8,78 @@ const app = express();
 const port = 8000;
 
 const largeImages = [
-    'https://media.discordapp.net/attachments/1199262650912219146/1199270818274869278/7bde30a066c2e80b.gif?ex=65c1eefc&is=65af79fc&hm=de2645a9b516429600db0552213605962b99c3acd7537ff3cfc92ae76ed7b07f&=',
+    'https://media.discordapp.net/attachments/1204374463651774505/1204382384821112842/b49f97c788e4a665d04bb9c42ed605ab.gif?ex=65d48780&is=65c21280&hm=60b214f70bb8b7430858511d455b208d4785811a1169abbb30cfa1725412bfbf&=',
+    'https://media.discordapp.net/attachments/1204374463651774505/1204382385412636712/acc40089e0d25ea64759458de84a991f.gif?ex=65d48781&is=65c21281&hm=7f1b6e289ea03692619c940abcbf8384f45a609bb2f34d7ea726dd91270bb0a3&=',
+    'https://media.discordapp.net/attachments/1204374463651774505/1204382386226208838/39bf4328d684c393fdf0d3ffc693f2a2.gif?ex=65d48781&is=65c21281&hm=ddfdd949959349321344ce4410214024668fd19492779418ad32f9e1191a48d7&=',
+    'https://media.discordapp.net/attachments/1204374463651774505/1204382387426033665/c53704bd9faa4912736316e09de4b514.gif?ex=65d48781&is=65c21281&hm=f9d7af5cc00da1d55e88e026ee72b4d02eb8c513a9e20cdcb151a6baf401e099&=',
+    'https://media.discordapp.net/attachments/1204374463651774505/1204382388684329011/de9a5eaf9f130a2099efcaca88a9e6e0.gif?ex=65d48781&is=65c21281&hm=44cffa8dcb82f7f550b25d33bcec9ced8faffb538f4be1ba7f4857c750840228&=',
+    'https://media.discordapp.net/attachments/1204374463651774505/1204382389539835914/509d8813769fae32f19d93b176529e60.gif?ex=65d48782&is=65c21282&hm=28dafb88df9998c7b2056af0b303453cdda6034059cc93cd38ac71d8bb9fe396&='
     // Add more large image URLs as needed
 ];
 
 const stateTexts = [
-    '「 𝙽𝙸𝙶𝙷𝚃 𝙸𝙽 𝚃𝙷𝙴 𝚂𝙺𝚈 」',
-    '「 𝚃𝙷𝙴 𝙾𝙽𝙻𝚈 𝙻𝙸𝙵𝙴 」',
-    '「 𝙹𝙾𝙸𝙽 𝙳𝙸𝚂𝙲𝙾𝚁𝙳 」',
+    '「 𝚍𝚊𝚢𝚍𝚛𝚎𝚊𝚖𝚜 」'
     // Add more state texts as needed
 ];
+
+const nameTexts = [
+  '꒦꒷ 𝚍𝚊𝚢𝚍𝚛𝚎𝚊𝚖𝚜',
+  // Add more state texts as needed
+];
+
 
 let currentStateIndex = 0; // Index to track the current state text
 
 let currentLargeImageIndex = 0;
 
+let currentnameTextsIndex = 0;
+
 app.get('/', (req, res) => res.send('ทำงานเรียบร้อยแล้ว'))
 app.listen(port, () =>
-    console.log(`Your app is listening at http://localhost:${port}`)
+  console.log(`Your app is listening at http://localhost:${port}`)
 );
 
 client.on("ready", async () => {
-    var startedAt = Date.now();
-    console.log(`${client.user.username} เม็ดม่วงทำงานเรียบร้อยแล้ว !`);
+  var startedAt = Date.now();
+  console.log(`${client.user.username} เม็ดม่วงทำงานเรียบร้อยแล้ว !`);
 
-    setInterval(() => {
-        const currentTime = getCurrentTime();
-        const currentDate = getCurrentDate();
+  setInterval(() => {
+      const currentTime = getCurrentTime();
+      const currentDate = getCurrentDate();
 
-        const r = new Discord.RichPresence()
-            .setApplicationId('1121867777867788309')
-            .setType('STREAMING')
-            .setState(stateTexts[currentStateIndex])
-            .setName('۞ 𝙰𝚂𝚃𝚁𝙾 𝙵𝙰𝙼')
-            .setDetails(` ﹝ ⌚${currentTime} | 🖤 Ka  Ting - 𝓐$t๏r ﹞ `)
-            .setStartTimestamp(startedAt)
-            .setAssetsLargeText(`﹝ 📅 ${currentDate}  |  🛸 0 m/s ﹞`)
-            .setAssetsLargeImage(largeImages[currentLargeImageIndex])
-            .setAssetsSmallText('A$t๏r 🖤')
-            .addButton('🆔 Ting-𝓐$t๏r 👻  🛜', 'https://wetv.vip/th')
-            .addButton('🔱 👑  A$t๏r  👑 🔱', 'https://www.twitch.tv/discord')
+      const r = new Discord.RichPresence()
+          .setApplicationId('1121867777867788309')
+          .setType('STREAMING')
+          .setURL('https://youtu.be/sVaQQRx6-es?si=WddbMqrjlhmF6kF8')
+          .setState(stateTexts[currentStateIndex])
+          .setName(nameTexts[currentnameTextsIndex])
+          .setDetails(` ﹝ ⌚ ${currentTime} | 💬 ${client.user.username} ﹞ `)
+          .setStartTimestamp(startedAt)
+          .setAssetsLargeText(`﹝ 📅 ${currentDate}  | 🛸 0 m/s ﹞`)
+          .setAssetsLargeImage(largeImages[currentLargeImageIndex])
+          .setAssetsSmallText('🦊')
+          .addButton('My G796', 'https://discord.com/invite/xhqcSC5K4Y')
 
-        client.user.setActivity(r);
+      client.user.setActivity(r);
 
       currentLargeImageIndex = (currentLargeImageIndex + 1) % largeImages.length;
       currentStateIndex = (currentStateIndex + 1) % stateTexts.length;
-    }, 1000); // Change large image and state text every 1 second
+      currentnameTextsIndex = (currentnameTextsIndex + 1) % nameTexts.length;
+  }, 3000); // Change large image and state text every 1 second
 });
 
 function getCurrentDate() {
-    const a = new Date(Date.now());
-    const c = { timeZone: "Asia/Bangkok", day: "2-digit", month: "2-digit", year: "numeric" };
-    const formattedDate = a.toLocaleDateString("en-US", c);
-    const [month, day, year] = formattedDate.split('/');
-    return `${day}/${month}/${year}`;
+  const a = new Date(Date.now());
+  const c = { timeZone: "Asia/Bangkok", day: "2-digit", month: "2-digit", year: "numeric" };
+  const formattedDate = a.toLocaleDateString("en-US", c);
+  const [month, day, year] = formattedDate.split('/');
+  return `${day}/${month}/${year}`;
 }
 
 function getCurrentTime() {
-    const a = new Date(Date.now());
-    const c = { timeZone: "Asia/Bangkok", hour: "numeric", minute: "numeric", hour12: false };
-    return a.toLocaleTimeString("th-TH", c);
+  const a = new Date(Date.now());
+  const c = { timeZone: "Asia/Bangkok", hour: "numeric", minute: "numeric", hour12: false };
+  return a.toLocaleTimeString("th-TH", c);
 }
 
 client.login(process.env.token);
